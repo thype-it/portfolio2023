e9b75f6eca6ebd2bba96ee54185d0d2e1aa7aee3
thype-it <illit.michal@gmail.com>
2023-09-21 12:41:05 +1000

    environment variables setup

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -1,4 +1,1 @@
-export const endpoint =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_URL
-    : process.env.API_URL;
+export const endpoint = `${process.env.NEXT_PUBLIC_URL}/api`;
